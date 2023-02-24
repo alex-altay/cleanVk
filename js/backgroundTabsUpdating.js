9a4737eeac0f10b0fcb46b5f43e98c148dedecd3
Alex <uliza.fonar.apteka@gmail.com>
2023-02-24 11:31:15 +0400

    Adds Recommended Video filter

--- js/backgroundTabsUpdating.js
+++ js/backgroundTabsUpdating.js
@@ -9,6 +9,7 @@ const options = {
   recommendedFriends: false,
   recommendedGroups: false,
   recommendedNarratives: false,
+  recommendedVideos: false,
   groupsReposts: false,
   likesCounter: false,
 };
